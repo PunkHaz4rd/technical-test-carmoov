fcd160326e0c27f0e06e790e66d3ff74e7e3848d
Romain <PunkHaz4rd@users.noreply.github.com>
2018-02-17 11:03:25 +0100

    Updating server.js

--- server.js
+++ server.js
@@ -40,13 +40,13 @@ app.post('/mailmeagif', (req, res) => {
   let giphyApiKey = 's9IFSGbx3LYI2rUvVRPItuAZWAeWSeZ9'
   let emailTest = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
   
-  if (!req.param.email) {
+  if (!req.query.email) {
     res.status(406).json({'error': 'Email undefined'});
-  } else if (!emailTest.test(req.param.email)) {
+  } else if (!emailTest.test(req.query.email)) {
     res.status(406).json({'error': 'Email malformed'});
   } else {
-    let url = giphyBaseUrl + 'translate?api_key=' + giphyApiKey + req.param.search ? ('&s=' + req.param.search) : 'random';
-    sendMail(req.param.email, url, (err, info) => {
+    let url = giphyBaseUrl + 'translate?api_key=' + giphyApiKey + req.query.search ? ('&s=' + req.query.search) : 'random';
+    sendMail(req.query.email, url, (err, info) => {
       if (err) {
         res.status(500).send({'error': err});
       } else {
